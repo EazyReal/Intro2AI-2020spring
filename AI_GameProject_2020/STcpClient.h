4f7377d922d9e27aef01a81db7a22a15405cb0b3
yt lin <0312fs3@gmail.com>
2020-06-09 16:09:49 +0800

    AI HW1, FINAL

---
+++ AI_GameProject_2020/STcpClient.h
@@ -0,0 +1,169 @@
+﻿//
+//	Copyright © 2019 by Phillip Chang
+//
+#pragma once
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib,"Ws2_32.lib")
+
+#include <cstdio>
+#include <vector>
+
+SOCKET socketServer = INVALID_SOCKET;
+const char *infoServer[] = { "localhost", "8888" };
+/*
+	請將 idTeam 改成組別
+*/
+int idTeam = 41;
+
+class _WSAData {
+public:
+	_WSAData() {
+		if (WSAStartup(MAKEWORD(2, 2), &wsadata_) != 0) {
+			printf("WSAStartup Fail %d", WSAGetLastError());
+		}
+	}
+	~_WSAData() {
+		WSACleanup();
+	}
+private:
+	WSADATA wsadata_;
+}initializerWSA;
+
+SOCKET _CreateConnectSocket(const char* strIp, const char* strPort) {
+	addrinfo addrinfoHint, *listDestAddrinfo = NULL, *addrinfoIterator = NULL;
+	ZeroMemory(&addrinfoHint, sizeof(addrinfo));
+	addrinfoHint.ai_family = AF_INET;
+	addrinfoHint.ai_socktype = SOCK_STREAM;
+	addrinfoHint.ai_protocol = IPPROTO_TCP;
+	int errorApi = getaddrinfo(strIp, strPort, &addrinfoHint, &listDestAddrinfo);
+	if (errorApi != 0) return INVALID_SOCKET;
+	//	create socket
+	SOCKET socketResult = socket(listDestAddrinfo->ai_family, listDestAddrinfo->ai_socktype, listDestAddrinfo->ai_protocol);
+	if (socketResult == INVALID_SOCKET) {
+		freeaddrinfo(listDestAddrinfo);
+		return INVALID_SOCKET;
+	}
+	//	try connect
+	errorApi = SOCKET_ERROR;
+	addrinfoIterator = listDestAddrinfo;
+	while (addrinfoIterator != NULL) {
+		errorApi = connect(socketResult, addrinfoIterator->ai_addr, (int)addrinfoIterator->ai_addrlen);
+		if (errorApi != SOCKET_ERROR) break;
+		addrinfoIterator = addrinfoIterator->ai_next;
+	}
+	freeaddrinfo(listDestAddrinfo);
+	if (errorApi == SOCKET_ERROR) {
+		closesocket(socketResult);
+		socketResult = INVALID_SOCKET;
+	}
+	return socketResult;
+}
+bool _SendToSocket(SOCKET socketDest, size_t lengthSend, const BYTE* rbSend) {
+	return send(socketDest, (const char*)rbSend, (int)lengthSend, 0) == lengthSend;
+}
+bool _RecvFromSocket(SOCKET socketSrc, size_t lengthRecv, BYTE* rbRecv) {
+	return recv(socketSrc, (char*)rbRecv, (int)lengthRecv, MSG_WAITALL) == lengthRecv;
+}
+
+void _ConnectToServer(int cntRecursive = 0) {
+	if (cntRecursive > 3) {
+		printf("[Error] : maximum connection try reached!\n");
+		return;
+	}
+	while (socketServer == INVALID_SOCKET)
+		socketServer = _CreateConnectSocket(infoServer[0], infoServer[1]);
+
+	if (!_SendToSocket(socketServer, sizeof(int), (BYTE*)&idTeam)) {
+		closesocket(socketServer);
+		socketServer = INVALID_SOCKET;
+		_ConnectToServer(cntRecursive + 1);
+	}
+}
+
+void _ReconnectToServer() {
+	if (socketServer != INVALID_SOCKET) {
+		closesocket(socketServer);
+		socketServer = INVALID_SOCKET;
+	}
+
+	_ConnectToServer();
+}
+
+/*
+	取得當前遊戲狀態
+
+    return (stop_program), (id_package, board, is_black)
+    stop_program : True 表示當前應立即結束程式，False 表示當前輪到自己下棋
+    id_package : 當前棋盤狀態的 id，回傳移動訊息時需要使用
+    board : 當前棋盤狀態 board[i][j] 表示棋盤第 i row, j column 上擺的棋子，0 表示沒有棋子、1 表示黑子、2 表示白子
+    is_black : true 表示當前是輪到黑子，client 只有在輪到自己的時候才會收到遊戲狀態訊息
+*/
+bool GetBoard(int& id_package, std::vector<std::vector<int>>& board, bool& is_black) {
+	if (socketServer == INVALID_SOCKET) {
+		_ConnectToServer();
+		if (socketServer == INVALID_SOCKET) {
+			return true;
+		}
+	}
+	BYTE rbHeader[8];
+	if (!_RecvFromSocket(socketServer, 8, rbHeader)) {
+		printf("[Error] : Connection lose, trying to reconnect...\n");
+		closesocket(socketServer);
+		socketServer = INVALID_SOCKET;
+		return GetBoard(id_package, board, is_black);
+	}
+	int codeHeader = *((int*)rbHeader);
+	id_package = *((int*)(rbHeader + 4));
+	if (codeHeader == 0) return true;
+
+	board.resize(8);
+	for (int i = 0; i < 8; ++i) {
+		board[i].resize(8);
+		for (int j = 0; j < 8; ++j) {
+			if (!_RecvFromSocket(socketServer, 4, (BYTE*)&(board[i][j]))) {
+				printf("[Error] : Connection lose, trying to reconnect...\n");
+				closesocket(socketServer);
+				socketServer = INVALID_SOCKET;
+				return GetBoard(id_package, board, is_black);
+			}
+		}
+	}
+
+	BYTE rbBlack[4];
+	if (!_RecvFromSocket(socketServer, 4, rbBlack)) {
+		printf("[Error] : Connection lose, trying to reconnect...\n");
+		closesocket(socketServer);
+		socketServer = INVALID_SOCKET;
+		return GetBoard(id_package, board, is_black);
+	}
+	is_black = (*((int*)rbBlack)) == 1;
+
+	return false;
+}
+/*
+	向 server 傳達移動訊息
+	id_package : 想要回復的訊息的 id_package
+	listStep = <r, c>
+	r, c 表示要移動的棋子座標 (row, column) (zero-base)
+*/
+void SendStep(int id_package, std::vector<int>& Step) {
+	if (socketServer == INVALID_SOCKET) {
+		printf("[Error] : trying to send step before connection is established\n");
+		return;
+	}
+
+	std::vector<BYTE> rbData;
+	rbData.resize(16);
+	int* rbInsert = (int*)&(rbData[0]);
+	*rbInsert++ = 1;
+	*rbInsert++ = id_package;
+	*rbInsert++ = Step[0];
+	*rbInsert++ = Step[1];
+
+	if (!_SendToSocket(socketServer, rbData.size(), &(rbData[0]))) {
+		printf("[Error] : Connection lose, trying to reconnect\n");
+		_ReconnectToServer();
+	}
+}
---
+++ AI_GameProject_2020/STcpClient.py
@@ -0,0 +1,157 @@
+'''
+    Copyright © 2019 by Phillip Chang
+'''
+import socket
+
+socketServer = None
+infoServer = ["localhost", 8888]
+'''
+    *   請將 idTeam 改成組別    *
+'''
+idTeam = 41
+
+def _Connect(ip, port):
+    socketCurrent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    addrServer = (ip, port)
+    error = socketCurrent.connect_ex(addrServer)
+    if(error == 0):
+        return socketCurrent
+    
+    socketCurrent.close()
+    return None
+
+def _RecvUntil(socketRecv, cntByte):
+    if(socketRecv is None):
+        return None
+    try:
+        rbData = socketRecv.recv(cntByte)
+    except socket.error as _:
+        return None
+    
+    if(len(rbData) != cntByte):
+        return None
+
+    return rbData
+
+def _SendAll(socketSend, rbData):
+    if(socketSend is None):
+        return False
+    try:
+        resultSend = socketSend.sendall(rbData)
+    except socket.error as e:
+        print(e)
+        return False
+
+    return resultSend is None
+
+import struct
+
+def _ConnectToServer(cntRecursive = 0):
+    global socketServer
+    global infoServer
+    global idTeam
+
+    if(cntRecursive > 3):
+        print("[Error] : maximum connection try reached!")
+        return
+    while(socketServer is None):
+        socketServer = _Connect(infoServer[0], infoServer[1])
+
+    structHeader = struct.Struct("i")
+    rbHeader = structHeader.pack(idTeam)
+    if(not _SendAll(socketServer, rbHeader)):
+        socketServer.close()
+        socketServer = None
+        _ConnectToServer(cntRecursive + 1)
+
+def _ReconnectToServer():
+    global socketServer
+
+    if(socketServer is not None):
+        socketServer.close()
+        socketServer = None
+    
+    _ConnectToServer()
+
+'''
+    取得當前遊戲狀態
+
+    return (stop_program, id_package, board, is_black)
+    stop_program : True 表示當前應立即結束程式，False 表示當前輪到自己下棋
+    id_package : 當前棋盤狀態的 id，回傳移動訊息時需要使用
+    board : 當前棋盤狀態 board[i][j] 表示棋盤第 i row, j column 上擺的棋子，0 表示沒有棋子、1 表示黑子、2 表示白子
+    is_black : True 表示當前是輪到黑子，client 只有在輪到自己的時候才會收到遊戲狀態訊息
+'''
+def GetBoard():
+    global socketServer
+
+    if(socketServer is None):
+        _ConnectToServer()
+        if(socketServer is None):
+            return (True, 0, None, None)
+    
+    # recv
+    structHeader = struct.Struct("ii")
+    structItem = struct.Struct("i")
+
+    rbHeader = _RecvUntil(socketServer, structHeader.size)
+    if(rbHeader is None):
+        print("[Error] : connection lose, trying to reconnect...")
+        socketServer.close()
+        socketServer = None
+        return GetBoard()
+    
+    (codeHeader, id_package) = structHeader.unpack(rbHeader)
+    if(codeHeader == 0):
+        return (True, 0, None, None)
+    
+    board = []
+    for i in range(8):
+        board.append([])
+        for _ in range(8):
+            rbBoard = _RecvUntil(socketServer, structItem.size)
+            if(rbBoard is None):
+                print("[Error] : connection lose, trying to reconnect...")
+                socketServer.close()
+                socketServer = None
+                return GetBoard()
+            itemBoard = structItem.unpack(rbBoard)[0]
+            board[i].append(itemBoard)
+    
+    rbBlack = _RecvUntil(socketServer, structItem.size)
+    if(rbBlack is None):
+        print("[Error] : connection lose, trying to reconnect...")
+        socketServer.close()
+        socketServer = None
+        return GetBoard()
+    is_black = (structItem.unpack(rbBlack)[0] == 1)
+
+    return (False, id_package, board, is_black)
+
+'''
+    向 server 傳達移動訊息
+    id_package : 想要回復的訊息的 id_package
+    Step = (r, c)
+    r, c 表示要下的棋子座標 (row, column) (zero-base)
+
+    return 函數是否執行成功
+'''
+def SendStep(id_package, Step):
+    global socketServer
+
+    if(socketServer is None):
+        print("[Error] : trying to send step before connection is established")
+        return
+    
+    # send
+    structItem = struct.Struct("ii")
+
+    rbHeader = structItem.pack(1, id_package)
+
+    rbHeader += structItem.pack(Step[0], Step[1])
+
+    # retry once
+    if(not _SendAll(socketServer, rbHeader)):
+        print("[Error] : connection lose, trying to reconnect")
+        _ReconnectToServer()
